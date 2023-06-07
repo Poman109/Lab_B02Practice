9eeb1c2619826b79066668fafcd55c869ded6fd2
Poman109 <mancy109@hotmail.com>
2023-06-07 21:40:35 +0800

    Ex 3

--- src/main/java/com/B02_Practice/b02practice/PersonApi.java
+++ src/main/java/com/B02_Practice/b02practice/PersonApi.java
@@ -1,9 +1,9 @@
 package com.B02_Practice.b02practice;
 
+import com.sun.net.httpserver.HttpServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -14,10 +14,28 @@ public class PersonApi {
 
 
     @PostMapping ("/person/create")
-    public Person createPerson(@RequestBody Person person){
-        this.person.add(person);
+    public Person createPerson(@RequestBody Person people){
+        person.add(people);
+        return people;
+    }
+
+    @GetMapping("/person/all")
+    public ArrayList<Person> getPerson(){
         return person;
     }
+
+    @PutMapping("/person/update")
+    public Person updatePerson(@RequestBody Person updatePerson){
+        for(Person person1: person){
+            if(person1.getHkid().equals(updatePerson.getHkid())){
+                person1.setFirstName(updatePerson.getFirstName());
+                person1.setLastName(updatePerson.getLastName());
+                return person1;
+            }
+        }
+        throw new NotFoundPeopleException();
+    }
+
 
 
 
